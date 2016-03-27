a5d7fd18650ab3953996b82a8736234a0598eea3
m5031 <1804662300@qq.com>
2016-03-27 11:51:38 +0800

    Delete start-celery-workers.sh

--- start-celery-workers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pkill -9 celeryd 2>/dev/null
-rm -rf /tmp/smart_programmer/assignment*
-python manage.py celery worker --concurrency=5 --loglevel=INFO 2>&1 | tee smart_programmer/log/task.log
